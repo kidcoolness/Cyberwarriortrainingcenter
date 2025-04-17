dde061c78a106aeedc59a5fe068f6b9bfc69cdb1
Ethan Chartrand <64283627+kidcoolness@users.noreply.github.com>
2025-04-17 02:48:45 +0000

    2

--- render_build.sh
+++ render_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+echo "Installing Dependencies"
+pip install -r requirements.txt
 
 echo "🧭 Setting Flask environment variables..."
 export FLASK_APP=wsgi.py
 export FLASK_ENV=production
 
 echo "📂 Ensuring correct working directory..."
-
-ls
 
 echo "🔥 Running database migrations..."
 flask db upgrade -d migrations
